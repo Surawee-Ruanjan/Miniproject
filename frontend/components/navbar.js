73492fd1facbd953085ceabb18c2050b210a7c8b
Surawee Ruanjan <32189952+Surawee-Ruanjan@users.noreply.github.com>
2021-04-25 13:23:48 +0700

    Initial commit

---
+++ frontend/components/navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+const Navbar = () => (
+    <div>
+        <Link href="/"><a> <b>Home </b></a></Link> |
+        <Link href="/register"><a> <b>Register</b> </a></Link>  |
+        <Link href="/login"><a> <b>Login</b> </a></Link> |
+        <Link href="/profile"><a> <b>Profile</b> </a></Link> | 
+        <Link href="/studentedit"><a> <b>Student</b> </a></Link> |
+        <Link href="/getConfig"><a><b> Config</b> </a></Link> | 
+        <Link href="/logout"><a> <b>Logout</b> </a></Link> 
+        
+    </div>
+)
+
+export default Navbar
